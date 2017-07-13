bdbc819011121cdce6da32b635d03c9c2cd18122
Maxim Bilan <maximb.mail@gmail.com>
2017-07-13 14:13:31 +0300

    Added project template

---
+++ SwiftyAccountKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyAccountKit
+//
+//  Created by Maxim on 7/13/17.
+//  Copyright © 2017 Maxim Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
