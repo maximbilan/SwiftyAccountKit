9c5e936f8190561968d8e533312ad478df13bd3b
Maxim Bilan <maximb.mail@gmail.com>
2017-07-13 14:40:25 +0300

    Added UI

--- SwiftyAccountKit/ViewController.swift
+++ SwiftyAccountKit/ViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
+	@IBAction func loginButtonAction(_ sender: UIButton) {
 	}
-
-	override func didReceiveMemoryWarning() {
-		super.didReceiveMemoryWarning()
-		// Dispose of any resources that can be recreated.
+	
+	@IBAction func phoneButtonAction(_ sender: UIButton) {
 	}
-
-
+	
 }
 
