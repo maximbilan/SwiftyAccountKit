463136b40b0f8d48a02ccff4ace406b715f728bb
Maxim Bilan <maximb.mail@gmail.com>
2017-07-13 14:17:20 +0300

    Added SwiftlyAccountKit.swift

---
+++ SwiftyAccountKit/Sources/SwiftlyAccountKit.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftlyAccountKit.swift
+//  SwiftyAccountKit
+//
+//  Created by Maxim on 7/13/17.
+//  Copyright © 2017 Maxim Bilan. All rights reserved.
+//
+
+import Foundation
+
+final class SwiftlyAccountKit: NSObject {
+	
+	// MARK: - Shared Instance
+	
+	static let shared = SwiftlyAccountKit()
+
+}
